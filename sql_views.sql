57327c3f21051f970132f258cbdec77d8cce62c6
LisoNum <timkasper06@gmail.com>
2026-01-29 15:06:10 +0100

    added more views for the diffrent ability bindings

--- sql_views.sql
+++ sql_views.sql
@@ -9,6 +9,24 @@ CREATE OR REPLACE VIEW Verlierer (Name, Farbe, Team_ID)
 AS SELECT c.Name, t.FARBE, t.TEAM_ID FROM SPIELMODUS s, TEAM t, CHAMPION c
 WHERE s.SPIEL_ID = t.SPIEL_ID and t.TEAM_ID = c.Team_ID and s.Gewinner != t.FARBE;
 
--- CREATE OR REPLACE VIEW Ultimates (Name, Champion_Name, Schaden, Buff)
--- AS SELECT f.Name, f.Champion_Name, feff.Schaden, feff.Buff FROM FAEHIGKEIT.f, FAEHIGKEIT_EFFEKT.feff 
--- WHERE f.Belegung = 'R' and f.Faehigkeit_Name = feff.Faehigkeit_Name;
+CREATE OR REPLACE VIEW Item_Info (Name, Champion_Name, Schaden, Buff, Belegung)
+AS SELECT f.Name, f.Champion_Name, feff.Schaden, feff.Buff, f.Belegung FROM FAEHIGKEIT f, FAEHIGKEIT_EFFEKT feff  
+WHERE f.NAME = feff.Faehigkeit_Name;
+
+CREATE OR REPLACE VIEW  Q_Ability (Name, Champion_Name, Schaden, Buff)
+AS SELECT f.Name, f.Champion_Name, feff.Schaden, feff.Buff FROM FAEHIGKEIT f, FAEHIGKEIT_EFFEKT feff  
+WHERE f.NAME = feff.Faehigkeit_Name and f.BELEGUNG = 'Q';
+
+CREATE OR REPLACE VIEW  W_Ability (Name, Champion_Name, Schaden, Buff)
+AS SELECT f.Name, f.Champion_Name, feff.Schaden, feff.Buff FROM FAEHIGKEIT f, FAEHIGKEIT_EFFEKT feff  
+WHERE f.NAME = feff.Faehigkeit_Name and f.BELEGUNG = 'W';
+
+CREATE OR REPLACE VIEW  E_Ability (Name, Champion_Name, Schaden, Buff)
+AS SELECT f.Name, f.Champion_Name, feff.Schaden, feff.Buff FROM FAEHIGKEIT f, FAEHIGKEIT_EFFEKT feff  
+WHERE f.NAME = feff.Faehigkeit_Name and f.BELEGUNG = 'E';
+
+CREATE OR REPLACE VIEW  R_Ability (Name, Champion_Name, Schaden, Buff)
+AS SELECT f.Name, f.Champion_Name, feff.Schaden, feff.Buff FROM FAEHIGKEIT f, FAEHIGKEIT_EFFEKT feff  
+WHERE f.NAME = feff.Faehigkeit_Name and f.BELEGUNG = 'R';
+
+COMMIT;
